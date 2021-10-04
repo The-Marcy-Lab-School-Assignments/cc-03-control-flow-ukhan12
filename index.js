e5aff54739a567477fd96fbbb918b0c11688f517
uzma <ubkhan3@gmail.com>
2021-10-04 14:12:24 +0000

    Complete all functions including bonus

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 //1
-function countToTen(){
-  
+function countFromOne(num){
+  for(let i=1; i<=num; i++){
+      console.log(i);
+  }
 }
 
 //2
-function countFromOne(){
-  
+function countEveryOdd(num){
+  for(let i=1; i<=num; i++){
+      if(i%2 > 0){
+          console.log(i);
+      }
+  }
 }
 
 //3
-function countEveryEven(){
+function isNegative(num){
+    if(num > 0 ){
+        return false;
+    }else
+        return true;
   
 }
 
 //4
-function countEveryOdd(){
-  
+function betweenFiveAndTwenty(num){
+  if(num >= 5 && num <= 20){
+      return true;
+  }else
+    return false;
 }
 
 //5 
-function countEvens(){
-  
+function isAllLowerCase(str){
+  if(str === str.toLowerCase()) {
+      return true;
+ } else
+    return false;
 }
 
+//bonus
+function countMultiplesOfFive(arr){
+    let count = 0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] % 5 === 0){
+            count++
+        }
+    }
+    return count;
+}
+
+//countFromOne(10);
+//countEveryOdd(11);
+//console.log(isNegative(-22));
+//console.log(betweenFiveAndTwenty(21));
+//console.log(isAllLowerCase('gdfHBJhbkjBKHc'));
+console.log(countMultiplesOfFive([1, 5, 6, 10, 11, 15]));
